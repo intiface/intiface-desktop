50fe75c3410c468768313659559097b7094a9ffa
Kyle Machulis <kyle@nonpolynomial.com>
2019-01-21 00:40:40 -0800

    fix: Fix issue with postcss config location not being found
    Workspace setup screws up postcss config file assumptions, so we have
    to set it explicitly in the webpack config for vue-cli.
    
    Fixes #2

--- packages/intiface/vue.config.js
+++ packages/intiface/vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  lintOnSave: false
-}
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      postcss: {
+        options: {
+          config: {
+            // This rule needs to be set so that yarn workspaces will work,
+            // otherwise postcss will complain about vuetify requirements in the
+            // main node_modules directory (since postcss config is in
+            // packages/intiface, not in project root).
+            path: './postcss.config.js',
+          }
+        }
+      }
+    }
+  }
+};
