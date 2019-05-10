f38b64142826742487391a8a9f83ca9703772a11
Kyle Machulis <kyle@nonpolynomial.com>
2019-05-09 22:22:30 -0700

    chore: Add some simple content to the Home page

--- packages/intiface/src/components/HomePanel/HomePanel.ts
+++ packages/intiface/src/components/HomePanel/HomePanel.ts
@@ -1,6 +1,11 @@
 import Vue from "vue";
+import * as package_info from "../../../package.json";
 import { Component } from "vue-property-decorator";
 
 @Component({})
 export default class HomePanel extends Vue {
+
+  public get Version(): string {
+    return package_info.version;
+  }
 }
--- packages/intiface/src/components/HomePanel/HomePanel.vue
+++ packages/intiface/src/components/HomePanel/HomePanel.vue
@@ -1,6 +1,25 @@
 <template>
   <v-container>
     <h1>Intiface Desktop Home</h1>
+    <b>Welcome to Intiface Desktop version {{ Version }}!</b>
+    <br/><br/>
+    <p>This is the new Home page, which at some point will be filled with lots of sex tech related content.</p>
+    <p>However, since we're still in the early days of development, right now you'll have to settle for boring static content.</p>
+    <br/>
+    <b>Things to do with Intiface (all links will open in web browser)</b>
+    <ul>
+      <li><a href="https://playground.buttplug.world">Buttplug Playground (Hardware Control, Web)</a></li>
+      <li><a href="https://syncydink.buttplug.world">Syncydink (Movie Sync/Playback, Web)</a></li>
+      <li><a href="https://github.com/FredTungsten/ScriptPlayer">ScriptPlayer (Movie Sync/Playback, Windows Only)</a></li>
+      <li><a href="http://www.realtouchscripts.com/download/file.php?id=2470">JoyFunScripter (Movie Sync Script Creator, Windows Only, Download Requires Free Account)</a></li>
+      <li>Game Vibration Router 2.0 (Video Game Hardware Control, Windows Only, Coming soon!)</li>
+    </ul>
+    <br/>
+    <b>Useful Websites</b>
+    <ul>
+      <li><a href="https://reddit.com/r/sextoys">Reddit r/sextoys Community</a></li>
+      <li><a href="https://realtouchscripts.com">RealTouch Scripts Movie Scripting Community</a></li>
+    </ul>
   </v-container>
 </template>
 
