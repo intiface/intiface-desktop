c95b387ae344de797f8c3c2700ca3463462d9b0e
Kyle Machulis <kyle@nonpolynomial.com>
2020-09-07 16:22:37 -0700

    chore: Open external links in external browser

--- packages/intiface/src/background.ts
+++ packages/intiface/src/background.ts
@@ -1,7 +1,7 @@
 
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, shell } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import { SetupElectronIntifaceServer, ShutdownElectronIntifaceServer } from "./utils/ElectronIntifaceInitializer";
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
@@ -97,96 +97,12 @@ if (isDevelopment) {
   }
 }
 
-/*
-"use strict";
-
-import { app, protocol, BrowserWindow, shell } from "electron";
-import {
-  createProtocol,
-  installVueDevtools,
-} from "vue-cli-plugin-electron-builder/lib";
-import { SetupElectronIntifaceServer, ShutdownElectronIntifaceServer } from "./utils/ElectronIntifaceInitializer";
-const isDevelopment = process.env.NODE_ENV !== "production";
-
-// Keep a global reference of the window object, if you don"t, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win: BrowserWindow | null;
-
-// Standard scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([{ scheme: "app", privileges: { standard: true, secure: true } }]);
-function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
-
-  win.webContents.addListener('will-navigate', function(event, url) {
-    event.preventDefault();
+app.on('web-contents-created', (e, contents) => {
+  contents.on('new-window', (e, url) => {
+    e.preventDefault();
     shell.openExternal(url);
   });
-
-  SetupElectronIntifaceServer(win).then(() => {
-    if (process.env.WEBPACK_DEV_SERVER_URL) {
-      // Load the url of the dev server if in development mode
-      win!.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
-      if (!process.env.IS_TEST) {
-        win!.webContents.openDevTools();
-      }
-    } else {
-      createProtocol("app");
-      // Load the index.html when not in development
-      win!.loadURL("app://./index.html");
-    }
-
-    win!.on("closed", () => {
-      win = null;
-    });
-  }).catch((err: Error) => console.log(err));
-}
-
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  //if (process.platform === "darwin") {
-  app.quit();
-  //}
+  contents.on('will-navigate', (e, url) => {
+    if (url !== contents.getURL()) e.preventDefault(), shell.openExternal(url);
+  });
 });
-
-app.on("will-quit", () => {
-  ShutdownElectronIntifaceServer();
-});
-
-app.on("activate", () => {
-  // On macOS it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    await installVueDevtools();
-  }
-
-  createWindow();
-});
-
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
-  if (process.platform === "win32") {
-    process.on("message", (data) => {
-      if (data === "graceful-exit") {
-        app.quit();
-      }
-    });
-  } else {
-    process.on("SIGTERM", () => {
-      app.quit();
-    });
-  }
-}
-*/
