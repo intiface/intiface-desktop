2c08874fe99fbc742508ecd8e110d680e6ec86b5
Kyle Machulis <kyle@nonpolynomial.com>
2020-09-07 16:22:57 -0700

    chore: Update Home panel text

--- packages/intiface/src/components/HomePanel/HomePanel.vue
+++ packages/intiface/src/components/HomePanel/HomePanel.vue
@@ -2,26 +2,73 @@
   <v-container>
     <h1>Intiface Desktop Home</h1>
     <b>Welcome to Intiface Desktop version {{ Version }}!</b>
-    <br/><br/>
-    <p>This is the Home page, which at some point will be filled with lots of sex tech related content.</p>
-    <p>However, since we're still in the early days of development, right now you'll have to settle for boring static content.</p>
+    <v-row>
+    <v-col>
+    <h2>NEWS</h2>
+    <p>
+      After no releases since May 2019, v17 is the new, upgraded version of
+      Intiface. Functionally, it's mostly the same, but it now defaults to a new
+      Rust engine on Windows, macOS, and Linux.
+    </p>
+    <p>Some things to note:</p>
+    <ul>
+      <li>On any platform, if you're having problems, there is now a help panel
+      where you can get links to places to ask for support (or contact support
+      directly in this app).</li>
+      <li>If you are a windows user having issues with the Rust engine (i.e.
+      missing hardware, crashes, etc), You can switch back to the old C# engine
+      in the Settings menu. Support for this will end at some point in the
+      future, but right now the rust engine is new and needs testing outside of
+      my development machine.</li>
+      <li>If you're a macOS/Linux user having issues with the new Rust engine,
+      please contact support. The old Node engine was buggier than Rust is
+      currently and is considered dead.</li>
+    </ul>
     <br/>
+    <p>
+      I realize it's not really been obvious what Intiface Desktop is to be used
+      for, and that's because I never really thought this through well. On top
+      of that, due to some other parts of Buttplug and Intiface requiring large
+      amounts of reworking, apps like Intiface Desktop and Syncydink have been
+      neglected for a while. I'm hoping to start setting a direction for
+      Intiface, as well as adding new features, including:
+    </p>
+    <ul>
+      <li>A live updating home page with new apps/announcements</li>
+      <li>Device testing and configuration for users</li>
+      <li>A device simulator for developers</li>
+      <li>App/Game/Etc lists</li>
+      <li>
+        Intiface Desktop App hosting/launching (think something like
+        Steam/Itch.io, though not sure it'll be a store)
+      </li>
+    </ul>
+    <br />
+    <p>Thanks for using Buttplug/Intiface!</p>
+    <p>- qDot</p>
+    <br />
+    </v-col>
+    <v-col>
     <b>Things to do with Intiface (all links will open in web browser)</b>
     <ul>
-      <li><a href="https://playground.buttplug.world">Buttplug Playground (Hardware Control, Web)</a></li>
-      <li><a href="https://syncydink.buttplug.world">Syncydink (Movie Sync/Playback, Web)</a></li>
-      <li><a href="https://github.com/FredTungsten/ScriptPlayer">ScriptPlayer (Movie Sync/Playback, Windows Only)</a></li>
-      <li><a href="http://www.realtouchscripts.com/download/file.php?id=2470">JoyFunScripter (Movie Sync Script Creator, Windows Only, Download Requires Free Account)</a></li>
-      <li><a href="https://intiface.com/ghr">Intiface Game Haptics Router</a></li>
+      <li>
+        <a href="https://playground.buttplug.world">Buttplug Playground (Hardware Control, Web)</a>
+      </li>
+      <li>
+        <a href="https://syncydink.buttplug.world">Syncydink (Movie Sync/Playback, Web)</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/FredTungsten/ScriptPlayer"
+        >ScriptPlayer (Movie Sync/Playback, Windows Only)</a>
+      </li>
+      <li>
+        <a href="https://intiface.com/ghr">Intiface Game Haptics Router</a> (Doesn't actually use Intiface Desktop yet >.> )
+      </li>
     </ul>
-    <br/>
-    <b>Useful Websites</b>
-    <ul>
-      <li><a href="https://metafetish.club">Metafetish Forums</a> - Good for support questions</li>
-      <li><a href="https://discord.buttplug.io">Metafetish Discord</a> - Good for support questions</li>
-      <li><a href="https://reddit.com/r/sextoys">Reddit r/sextoys Community</a></li>
-      <li><a href="https://realtouchscripts.com">RealTouch Scripts Movie Scripting Community</a></li>
-    </ul>
+    <br />
+    </v-col>
+    </v-row>
   </v-container>
 </template>
 
